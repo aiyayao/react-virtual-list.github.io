c8e479bc8e1d1fe3419f868c08351c28daf21b8f
renjie <rj.aiyayao@gmail.com>
2020-07-09 18:14:41 +0800

    init project

---
+++ precache-manifest.0484f9bfc1cd34607cb569a6767aad05.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2617376d2aeff844156fe7f107f86b43",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cbac41db8cc1cd86453b",
+    "url": "/static/css/main.449a4754.chunk.css"
+  },
+  {
+    "revision": "65172a64d41b1bead957",
+    "url": "/static/js/2.bdf00125.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.bdf00125.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cbac41db8cc1cd86453b",
+    "url": "/static/js/main.3a6822dd.chunk.js"
+  },
+  {
+    "revision": "22d8adbfd366310dcb06",
+    "url": "/static/js/runtime-main.40f7f1bb.js"
+  }
+]);
